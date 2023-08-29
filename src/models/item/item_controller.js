e43e0815ae60a200da32f50bb1a3fff8759a1063
André Alencar <andrealencardosantos@gmail.com>
2023-08-29 16:44:45 -0300

    MappingItem criado

--- src/models/item/item_controller.js
+++ src/models/item/item_controller.js
@@ -35,12 +35,12 @@ const deleteItemById = async (req, res) => {
 const updateItemById = async (req, res) => {
   try {
     const id = parseInt(req.params.id);
-    const { order_id, product_id, price, quantity } = req.body;
+    const mappedItem = mapItemFields(req.body);
     await Item.update(id, {
-      order_id,
-      product_id,
-      price,
-      quantity,
+      order_id: mappedItem.order_id,
+      product_id: mappedItem.product_id,
+      price: mappedItem.price,
+      quantity: mappedItem.quantity,
     });
     res.status(200).json({ message: "Item atualizado com sucesso" });
   } catch (error) {
@@ -48,6 +48,15 @@ const updateItemById = async (req, res) => {
     res.status(500).json({ error: "Erro ao atualizar item" });
   }
   res.status(204).end();
+};
+
+const mapItemFields = (data) => {
+  return {
+    order_id: id_pedido,
+    product_id: data.id_produto,
+    price: data.preco,
+    quantity: data.quantidade,
+  };
 };
 
 module.exports = {
